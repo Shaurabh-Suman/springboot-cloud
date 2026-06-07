80c3a646a7c7cf6d86b4dcf251ec2dce9a9fa2d9
Shaurabh Suman <shaurabh.suman0001@gmail.com>
2026-06-07 17:15:52 +0530

    ResponseEntity implementation for customized response.

--- src/main/java/com/spring/cloud/app/controller/FibonacciController.java
+++ src/main/java/com/spring/cloud/app/controller/FibonacciController.java
@@ -1,7 +1,12 @@
 package com.spring.cloud.app.controller;
 
+import org.springframework.http.ResponseEntity;
+
 import java.util.List;
+import java.util.Map;
 
 public interface FibonacciController {
-    List<Integer> getFibonacci(int n);
+  // ResponseEntity< List<Integer>> getFibonacci(int n);
+  ResponseEntity<Map<String, Object>> getFibonacci(int n);
+
 }
--- src/main/java/com/spring/cloud/app/controller/impl/FibonacciControllerImpl.java
+++ src/main/java/com/spring/cloud/app/controller/impl/FibonacciControllerImpl.java
@@ -3,22 +3,44 @@ package com.spring.cloud.app.controller.impl;
 import com.spring.cloud.app.controller.FibonacciController;
 import com.spring.cloud.app.service.FibonacciService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class FibonacciControllerImpl implements FibonacciController {
     @Autowired
     FibonacciService fibonacciService;
 
+    /*
+     @Override
+    @GetMapping("/fibonacci")
+    public ResponseEntity<List<Integer>> getFibonacci(@RequestParam int n) {
+        return ResponseEntity.ok( fibonacciService.generateFibonacci(n));
+    }
+     */
+
     @Override
     @GetMapping("/fibonacci")
-    public List<Integer> getFibonacci(@RequestParam int n) {
+    public ResponseEntity<Map<String, Object>> getFibonacci(
+            @RequestParam int n) {
 
-        return fibonacciService.generateFibonacci(n);
+        List<Integer> fibonacciList =
+                fibonacciService.generateFibonacci(n);
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("message",
+                "Fibonacci series generated successfully");
+        response.put("data", fibonacciList);
+
+        return ResponseEntity.ok(response);
     }
 }
